337fdbe8dd1117839841711034fab5e1b92aa506
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-01-04 11:44:39 +0300

    DataAccess Layer Added

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -16,7 +16,7 @@ namespace Entities.Concrete
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public string Plaka { get; set; }
+        public string? Plaka { get; set; }
         public string Type { get; set; }
         public bool IsActive { get; set; }
         public bool IsDeleted { get; set; }
